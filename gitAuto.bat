44e98e8d4bf54a4e5a42ba964e9605ab4f1a9c0f
Kostas Klimantakis <kklimantakis@gmail.com>
2024-03-07 11:43:37 +0200

    2024-03-07  11:43 AM

---
+++ gitAuto.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %~dp0
+git add .
+FOR /F "tokens=*" %%i IN ('date /t') DO set DATEVAR=%%i
+FOR /F "tokens=*" %%i IN ('time /t') DO set TIMEVAR=%%i
+git commit -m "%DATEVAR% %TIMEVAR%"
+git push origin main
